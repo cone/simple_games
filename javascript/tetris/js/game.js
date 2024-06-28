35b7dde989f5eaf11b607087cd807b502994dcd9
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2024-06-28 12:56:51 -0600

    Fix indentation

--- javascript/tetris/js/game.js
+++ javascript/tetris/js/game.js
@@ -1,27 +1,27 @@
 class Game {
-    constructor() {
-      this.board = new Board();
-      this.printer = new ScreenPrinter();
-      this.currentShape = new Shape();
-      this.interval = null;
-    }
+  constructor() {
+    this.board = new Board();
+    this.printer = new ScreenPrinter();
+    this.currentShape = new Shape();
+    this.interval = null;
+  }
 
-    start() {
-      const reprint = () => {
-        this.printer.clear();
-        this.printer.drawBoard(this.board);
-        this.currentShape.moveDown();
-        this.printer.drawShape(this.currentShape);
-      }
-      reprint.bind(this);
-      this.interval = setInterval(function() {
-        reprint();
-      }, 1000);
+  start() {
+    const reprint = () => {
+      this.printer.clear();
+      this.printer.drawBoard(this.board);
+      this.currentShape.moveDown();
+      this.printer.drawShape(this.currentShape);
     }
+    reprint.bind(this);
+    this.interval = setInterval(function() {
+      reprint();
+    }, 1000);
+  }
 
-    stop() {
-      clearInterval(this.interval);
-    }
+  stop() {
+    clearInterval(this.interval);
+  }
 }
 
-(new Game()).start();
+// (new Game()).start();
--- javascript/tetris/js/screen_printer.js
+++ javascript/tetris/js/screen_printer.js
@@ -3,40 +3,40 @@ const SQUARE_LINE_WIDTH = 1;
 const SQUARE_STROKE_STYLE = "black";
 
 class ScreenPrinter {
-    constructor() {
-      this.canvas = document.getElementById("screen");
-      this.ctx = this.canvas.getContext("2d");
-    }
+  constructor() {
+    this.canvas = document.getElementById("screen");
+    this.ctx = this.canvas.getContext("2d");
+  }
 
-    drawBoard(board) {
-      const rowSize = board.grid.length;
-      const colSize = board.grid[0].length;
-      for (let row = 0; row < rowSize; row++) {
-        for (let col = 0; col < colSize; col++) {
-          this.drawSquare(col, row, board.grid[row][col]);
-        }
+  drawBoard(board) {
+    const rowSize = board.grid.length;
+    const colSize = board.grid[0].length;
+    for (let row = 0; row < rowSize; row++) {
+      for (let col = 0; col < colSize; col++) {
+        this.drawSquare(col, row, board.grid[row][col]);
       }
     }
+  }
 
-    drawShape(shape) {
-      shape.getRealCoordinates().forEach(([col, row]) => {
-        this.drawSquare(col, row, shape.color);
-      });
-    }
+  drawShape(shape) {
+    shape.getRealCoordinates().forEach(([col, row]) => {
+      this.drawSquare(col, row, shape.color);
+    });
+  }
 
-    drawSquare(x = null, y = null, color = "cyan") {
-      if (!color) return;
+  drawSquare(x = null, y = null, color = "cyan") {
+    if (!color) return;
 
-      const xInPixels = x * SQUARE_SIZE;
-      const yInPixels = y * SQUARE_SIZE;
-      this.ctx.fillStyle = color;
-      this.ctx.fillRect(xInPixels, yInPixels, SQUARE_SIZE, SQUARE_SIZE);
-      this.ctx.lineWidth = SQUARE_LINE_WIDTH;
-      this.ctx.strokeStyle = SQUARE_STROKE_STYLE;
-      this.ctx.strokeRect(xInPixels, yInPixels, SQUARE_SIZE, SQUARE_SIZE);
-    }
+    const xInPixels = x * SQUARE_SIZE;
+    const yInPixels = y * SQUARE_SIZE;
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(xInPixels, yInPixels, SQUARE_SIZE, SQUARE_SIZE);
+    this.ctx.lineWidth = SQUARE_LINE_WIDTH;
+    this.ctx.strokeStyle = SQUARE_STROKE_STYLE;
+    this.ctx.strokeRect(xInPixels, yInPixels, SQUARE_SIZE, SQUARE_SIZE);
+  }
 
-    clear() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 }
